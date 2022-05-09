d55fa9a1209c03243b25930faae92ecc72541569
Mustafa Tariq <mustafa@tariqk.tech>
2022-05-09 14:31:44 -0400

    Forgot return statement.

--- twitter.py
+++ twitter.py
@@ -15,7 +15,7 @@ def collect_data(keyword):
     result = {"time":[], "text":[]}
     date_until = datetime.datetime.now()
     date_since = date_until - datetime.timedelta(days=5)
-    
+
     for tweet in tweepy.Cursor(api.search, 
                                q=keyword,
                                since = date_since, 
@@ -23,6 +23,7 @@ def collect_data(keyword):
                                lang="en").items():
         result["time"].append(tweet.created_at)
         result["text"].append(tweet.text)
+    return result
 
 def get_timeseries(keyword):
     """This specific function will return a json that looks like this
