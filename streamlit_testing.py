5e7cf877b58dc155780aee3545e091b85ada6250
Layale B <layalebazzi@gmail.com>
2022-05-12 09:26:52 -0400

    Major updates to tweet cleaner and data collection

--- streamlit_testing.py
+++ streamlit_testing.py
@@ -1,22 +1,26 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import twitter as tpy
 
-st.title('Twitter Sentiment Trend Analysis')
+st.title("Twitter Sentiment Trend Analysis")
 
-st.text('This app takes a keyword as input and returns the sentiment over time.')
-st.text('A histogram of most commonly used words and hashtags is also provided.')
+st.text("This app takes a keyword as input and returns the sentiment over time.")
+st.text("A histogram of most commonly used words and hashtags is also provided.")
 
 st.text_input("Enter keyword", key="keyword", value="cibc")
 
 keyword = st.session_state.keyword
 
-tweet_time_df, word_labels, word_counts, hashtags = tpy.get_timeseries(keyword)
+tweet_time_df, word_labels, word_counts, hashtag_labels, hashtag_counts = tpy.get_timeseries(keyword)
 
 st.line_chart(tweet_time_df)
 
 freq_df = pd.DataFrame({"Count":word_counts}, index = word_labels)
+hash_df = pd.DataFrame({"Count":hashtag_counts}, index = hashtag_labels)
 
+st.header("Common words associated with keyword")
 st.bar_chart(freq_df.head())
+
+st.header("Hashtags associated with keyword")
+st.bar_chart(hash_df.head())
 
--- twitter.py
+++ twitter.py
@@ -4,8 +4,6 @@ sentiment analysis on the data for the past 5 days.
 import datetime
 import re
 import tweepy
-import nltk
-import urllib.parse
 import numpy as np
 import pandas as pd
 from collections import Counter
@@ -16,38 +14,79 @@ from secret import * # Contains the OAuth authentication tokens.
 
 api = tweepy.Client(BEARER_TOKEN, CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-def collect_data(keyword):
-    result = {"time":[], "text":[]}
-    date_until = datetime.datetime.now()
-    date_start = date_until - datetime.timedelta(days = 5)
-    number_of_tweets = 100
-    # Will query up to 7 days maximum
+def sort_freq(input_text_list):
+    """
+    sort_freq takes as input a list of words and returns two lists, 
+    word_counts which is the count of the word in the word_labels list.
+    Both lists are sorted in descending order.
+    """
+    
+    counts = Counter(" ".join(input_text_list).split(" "))
 
-    keyword = keyword + " lang:en"
+    # Sorting distribution from highest to lowest frequency
+    labels, values = zip(*counts.items())
+    word_labels = np.array(labels)[np.argsort(values)[::-1]]
+    word_counts = np.array(values)[np.argsort(values)[::-1]]
 
-    tweets = api.search_recent_tweets(query=keyword,
-                                max_results=number_of_tweets,
-                                end_time=date_until,
-                                start_time=date_start,
-                                tweet_fields=["text","created_at"])
+    return word_labels, word_counts
 
+def moving_average(x, w=3):
+    """
+    moving_average calculates the moving average of an array of numbers using
+    convolution. The input array is convolved with a smaller array of ones,
+    equivalent to a cumulative sum, then divided by the length of the array of
+    ones. 
+    """
+    return np.convolve(x, np.ones(w), 'same') / w
+
+def collect_data(keyword, hours_num):
+    """
+    collect_data uses the Twitter API via tweepy module to collect tweets based
+    on a search query. WIth current Essential Access, only Twitter API v2 can
+    be used. Searches are limited to a maximum of 7 days before current day and
+    only 100 tweets can be accessed per search. This function collects 100 tweets
+    per hour and returns a dictionary with the raw text and timestamp.
+    """
+
+    date_start = []
     raw_tweets = []
     tweet_time = []
     
-    for twt in tweets.data:
-        raw_tweets.append(twt.text)
-        tweet_time.append(twt.created_at)
+    number_of_tweets = 100
     
-    result = {"time": tweet_time, "text":raw_tweets}
+    # Query
+    keyword = keyword + " lang:en"
 
-    return result
+    for date_idx in range(1,hours_num):
+
+        date_start = datetime.datetime.now() - datetime.timedelta(hours = date_idx)
+        date_until = date_start + datetime.timedelta(hours=1)
+
+        tweets = api.search_recent_tweets(query=keyword,
+                                    max_results=number_of_tweets,
+                                    end_time=date_until,
+                                    start_time=date_start,
+                                    tweet_fields=["text","created_at"])
+
+        
+        for twt in tweets.data:
+            raw_tweets.append(twt.text)
+            tweet_time.append(twt.created_at)
+    
+    result = {"time": tweet_time, "text": raw_tweets}
+
+    if result == {}:
+        return {"time": [" "], "text": [" "]}
+    else:
+        return result
 
 def clean_tweet(tweet):
     """
-    This function processes the tweet into a string array for NLP sentiment analysis. 
-    Also returns hashtags.   
+    clean_tweet processes the tweet into a list of words for NLP sentiment analysis. 
+    Also returns hashtags as list of words.   
     """
-
+    tweet = tweet.lower()
+    
     # Removing RT from tweet (can't figure out how to filter out retweets with Twitter APIv2)
     remove_RT = lambda x: re.compile('RT @').sub('@',x,count=1).strip() 
     tweet = remove_RT(tweet)
@@ -63,10 +102,6 @@ def clean_tweet(tweet):
     hashtags = re.findall(r"#(\w+)", tweet)
     tweet = re.sub("#[A-Za-z0-9_]+","", tweet)
 
-    # Converting uppercase characters
-    tweet = tweet.lower()
-
-    # Splitting up all words into array of strings
     tokens = tweet.split()
 
     # Removing punctuation
@@ -81,22 +116,13 @@ def clean_tweet(tweet):
     new_tweet = [w for w in token if w not in stop_words]
 
 
-    return ' '.join(new_tweet), hashtags
+    return ' '.join(new_tweet), ' '.join(hashtags)
 
 def analyze_tweets(input_tweets):
     """
-    This function returns statistics on processed set of tweets.
+    analyze_tweets returns statistics on processed set of tweets.
     Returns frequency data as well as sentiment analysis.
-    Returns only top 10 word occurrences because there is too much data.
     """
-    def moving_average(a, n=1):
-    
-        # Cumulative sum
-        ret = np.cumsum(a, dtype=float)
-
-        # Calculating mean
-        ret[n:] = ret[n:] - ret[:-n]
-        return ret[n - 1:] / n
 
     # Empty list to contain sentiment values
     all_sentiment = []
@@ -113,25 +139,22 @@ def analyze_tweets(input_tweets):
         # Values between are neutral
         all_sentiment.append(temp["compound"])
 
-    # Acquiring frequency distribution of words
-    print(input_tweets)
-    counts = Counter(" ".join(input_tweets).split(" "))
-
-    # Sorting distribution from highest to lowest frequency
-    labels, values = zip(*counts.items())
-    word_labels = np.array(labels)[np.argsort(values)[::-1]]
-    word_counts = np.array(values)[np.argsort(values)[::-1]]
+    word_labels, word_counts = sort_freq(input_tweets)
 
     rolling_sentiment = moving_average(all_sentiment)
 
     return rolling_sentiment, word_labels, word_counts
 
 def get_timeseries(keyword):
-
-    raw_tweet_data = collect_data(keyword)
-
+    """
+    get_timeseries takes as input a keyword from the frontend and returns
+    the a dictionary of sentiments over time (timestamps) and histogram data
+    for the words in the tweets as well as the used hashtags.
+    """
     processed_tweets = []
     hashtags = []
+    
+    raw_tweet_data = collect_data(keyword, 10)
     
     for idx in range(len(raw_tweet_data["text"])):
         temptweet, temphash = clean_tweet(str(raw_tweet_data["text"][idx]))
@@ -140,6 +163,11 @@ def get_timeseries(keyword):
 
     sentiments, word_labels, word_counts = analyze_tweets(processed_tweets)
 
+    # Removes empty entries
+    hashtags = list(filter(None, hashtags))
+
+    hashtag_labels, hashtag_counts = sort_freq(hashtags)
+
     time_tweets_df = pd.DataFrame({"Sentiment":sentiments}, index = raw_tweet_data["time"])
 
-    return time_tweets_df, word_labels, word_counts, hashtags
+    return time_tweets_df, word_labels, word_counts, hashtag_labels, hashtag_counts
