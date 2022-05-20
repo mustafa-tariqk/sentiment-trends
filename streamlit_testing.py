afa5f11e9727c08ff917d2d1afa774d30e8fc692
Mustafa Tariq <mustafa@tariqk.tech>
2022-05-20 11:06:08 -0400

    added some flavor

--- streamlit_testing.py
+++ streamlit_testing.py
@@ -10,10 +10,10 @@ st.set_page_config(page_title="Twitter Sentiment Trend Analysis", page_icon="üê
 st.image("CIBC x Innovation Logo - Simplified Grey.png")
 st.title("Twitter Sentiment Trend Analysis")
 
-st.markdown("Type in a keyword and see how people on twitter have felt about it!  \n\
-            The higher the point on the graph, the better people feel about the keyword.  \n\
-            We also share the most associated words and hashtags below the graph.")
-
+st.markdown("Type in a keyword and see how people on twitter \
+            have felt about it. The higher the point is the more \
+            positive the tweet is, the lower the point is the more \
+            negative the tweet is.")
 
 st.text_input("Enter keyword", key="keyword", value="cibc")
 
@@ -23,6 +23,7 @@ tweet_time_df, word_labels, word_counts, hashtag_labels, hashtag_counts = tpy.ge
 
 # Getting figure object handle (copy and to_numpy are used to get correct array dimensions)
 chart = tpy.get_line_chart(tweet_time_df)
+st.markdown("\# of Tweets: " + str(tweet_time_df.shape[0]))
 
 st.altair_chart(
     chart.interactive(),
@@ -35,11 +36,13 @@ hash_df = pd.DataFrame({"Count":hashtag_counts}, index = hashtag_labels)
 col1, col2 = st.columns(2)
 with col1:
     st.header("Associated Keywords")
+    st.markdown("The top 10 keywords that are most frequently used in the tweets")
     fig = tpy.get_bar_chart(freq_df.iloc[1:11])
     st.pyplot(fig)
 
 with col2:
     st.header("Associated Hashtags")
+    st.markdown("The top 10 hashtags that are most frequently used in the tweets")
     fig = tpy.get_bar_chart(hash_df.iloc[1:11])
     st.pyplot(fig)
 
